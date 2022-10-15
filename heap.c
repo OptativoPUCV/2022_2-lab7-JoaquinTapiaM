ffdb1d90e62e26485d228b3e70767a6501c4134f
Replit user <>
2022-10-15 20:53:37 +0000

    new try

--- heap.c
+++ heap.c
@@ -80,6 +80,7 @@ void heap_pop(Heap* pq){
   int j = 1;
   //si no tiene hijos se saldra del while
   while (j < pq->size){
+    printf("si\n");
     //caso que tenga 2 nodos hijos
     if(j+1 < pq->size){
       //al menos uno de los 2 hijos tiene mayor prioridad que el i 
